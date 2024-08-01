88cb952977772a59f5146a6eb168007951760627
chetanharjani <chetonix@gmail.com>
2024-08-01 20:44:43 +0530

    add bolierplate file for hirst painting

---
+++ main_boilerplate.py
@@ -0,0 +1,34 @@
+import turtle as t
+import random
+
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fast")
+screen = t.Screen()
+
+
+def random_color():
+    #return random rgb value from 0 to 255 as a tuple
+    pass
+
+tim.hideturtle()
+tim.penup()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+for _ in range(10):
+  for _ in range(10):
+     pass
+    #put dot with radius 20 and color random
+    
+    #put the turtle forward by 50
+    
+  #setheading 90, go forward 50, setheading 180, then forward by 500, and setheading 0
+  pass
+  
+  
+  
+  
+  
+screen.exitonclick()
